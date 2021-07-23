9a30ce7d22275d42b884035c8850341cabaa74a7
leehle radebe <leehle.radebe@gmail.com>
2021-07-23 23:41:31 +0200

    test

--- script/main.js
+++ script/main.js
@@ -45,30 +45,30 @@ fileIn.addEventListener("change", function() {
 // };
 
 
-// Webcam.set({
-//     width: 220,
-//     height: 190,
-//     image_format: 'jpeg',
-//     jpeg_quality: 100
-// });
-// Webcam.attach('#camera');
+Webcam.set({
+    width: 220,
+    height: 190,
+    image_format: 'jpeg',
+    jpeg_quality: 100
+});
+Webcam.attach('#camera');
 
-// takeSnapShot = function () {
-//     Webcam.snap(function (data_uri) {
-//     document.getElementById('snapShot').innerHTML = 
-//     '<img src="' + data_uri + '" width="70px" height="50px" />';
-//     });
-// }
+takeSnapShot = function () {
+    Webcam.snap(function (data_uri) {
+    document.getElementById('snapShot').innerHTML = 
+    '<img src="' + data_uri + '" width="70px" height="50px" />';
+    });
+}
 
-// takeSnapShot = function () {
-//     Webcam.snap(function (data_uri) {
-//     downloadImage('arun', data_uri);
-//     });
-// }
+takeSnapShot = function () {
+    Webcam.snap(function (data_uri) {
+    downloadImage('arun', data_uri);
+    });
+}
 
-// downloadImage = function (name, datauri) {
-//     var a = document.createElement('a');
-//     a.setAttribute('download', name + '.png');
-//     a.setAttribute('href', datauri);
-//     a.click();
-// }
+downloadImage = function (name, datauri) {
+    var a = document.createElement('a');
+    a.setAttribute('download', name + '.png');
+    a.setAttribute('href', datauri);
+    a.click();
+}
